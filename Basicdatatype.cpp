cd72370fc5a5030f7b74f4b2a7262b24cf90d0b0
Mohit Malviya <9685mohit@gmail.com>
2021-08-29 22:21:03 +0530

    gdgd

---
+++ Basicdatatype.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long long b;
+    float c;
+    double d ;
+    char e;
+    
+    cin>>a>>b>>e>>d>>c;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<e<<endl;
+    cout<<setprecision(3)<<d<<endl;
+    cout<<setprecision(6)<<c<<endl;
+    return 0;
+}
